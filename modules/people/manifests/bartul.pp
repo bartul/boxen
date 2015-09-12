e31cab3f2b70f0d8ea9d6df97295c9163964229a
Bartul Bonacic <bartul.bonacic@live.com>
2015-09-12 14:01:54 +0400

    docker in

--- modules/people/manifests/bartul.pp
+++ modules/people/manifests/bartul.pp
@@ -1,5 +1,6 @@
 class people::bartul {
   include vagrant  # requires vagrant module in Puppetfile
   include packer  # requires packer module in Puppetfile
+  include docker  # requires docker module in Puppetfile
  
 }
