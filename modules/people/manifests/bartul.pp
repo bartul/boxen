abe6480e27657ceb6cb11faf46d9eef6b1f3890c
Bartul Bonacic <bartul@Bartuls-MacBook-Pro.local>
2016-02-16 16:00:14 +0100

    Update

--- modules/people/manifests/bartul.pp
+++ modules/people/manifests/bartul.pp
@@ -2,5 +2,6 @@ class people::bartul {
   include vagrant  # requires vagrant module in Puppetfile
   include packer  # requires packer module in Puppetfile
   include docker  # requires docker module in Puppetfile
- 
+  include iterm2::stable
+  include zsh 
 }
